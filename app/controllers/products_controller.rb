0528fa83e302dcddca074d69dbafd16b89a8c266
samuel <sbacon803@gmail.com>
2022-08-12 11:46:24 -0600

    add show, update and destroy actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,6 +14,27 @@ class ProductsController < ApplicationController
     product.save
     render json: product.as_json
   end
+
+  def show
+    product = Product.find_by(id: params[:id])
+    render json: product.as_json
+  end
+
+  def update
+    product = Product.find_by(id: params[:id])
+    product.name = "Oile_heater_2"
+    product.price = params[:price] || product.price
+    product.image_url = params[:image_url] || product.image_url
+    product.description = params[:description] || product.description
+    product.save
+    render json: product.as_json
+  end
+
+  def destroy
+    product = Product.find_by(id: params[:id])
+    product.destroy
+    render json: { message: "product has been destroyed" }
+  end
 end
 
 # t.string "name"
