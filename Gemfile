1176f2df0404fa88ebdaef437dbb1458e219262b
samuel <sbacon803@gmail.com>
2022-08-13 18:10:36 -0600

    add jb gem, changed desc. to text, price to decimal, added quantity column, seeds dump

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ gem 'jb'
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
+gem 'seed_dump'
+
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-product = Product.new(name: "TV", description: "you watch it", price: 300, image_url: "https://i.pcmag.com/imagery/reviews/05USmNmGKiWMsLrCnbjnFRu-1..v1649174436.jpg")
-#product.save
-product = Product.new(name: "computer", description: "you code with it", price: 1200, image_url: "https://cdn.britannica.com/77/170477-050-1C747EE3/Laptop-computer.jpg")
-#product.save
-product = Product.new(name: "chair", description: "yyou sit on it", price: 45, image_url: "https://m.media-amazon.com/images/I/81J5r9dANGL._SL1500_.jpg")
-#product.save
+Product.create!([
+  {name: "lamp", description: "sheds light", price: "15.0", image_url: "https://target.scene7.com/is/image/Target/GUEST_9baad3f4-e6b5-4f77-97d9-b1e5d4d57d6a", quantity: nil},
+  {name: "TV", description: "you watch it", price: "300.0", image_url: "https://i.pcmag.com/imagery/reviews/05USmNmGKiWMsLrCnbjnFRu-1..v1649174436.jpg", quantity: nil},
+  {name: "chair", description: "yyou sit on it", price: "45.0", image_url: "https://m.media-amazon.com/images/I/81J5r9dANGL._SL1500_.jpg", quantity: nil},
+  {name: "table", description: "you put stuff on it", price: "30.0", image_url: "https://images.demandware.net/dw/image/v2/BBBV_PRD/on/demandware.static/-/Sites-master-catalog/default/dw04b3c19c/images/600000/606028.jpg?sfrm=jpg", quantity: nil},
+  {name: "Oile_heater_2", description: "heats oil", price: "200.0", image_url: "https://i5.walmartimages.com/asr/532e2708-be9a-4df8-854e-776ef1937389.82335f658c31c7055894278a30f6f9e7.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF", quantity: nil},
+  {name: "napkins", description: "wipes spills", price: "5.0", image_url: "https://t3.ftcdn.net/jpg/05/00/80/80/240_F_500808027_ifTfsf5LxuErtDjGnA1IopwF6B0ITQPT.jpg", quantity: nil},
+  {name: "better_napkins", description: "wipes spills", price: "5.0", image_url: "https://t3.ftcdn.net/jpg/05/00/80/80/240_F_500808027_ifTfsf5LxuErtDjGnA1IopwF6B0ITQPT.jpg", quantity: nil},
+  {name: "better_napkins", description: "wipes spills", price: "5.0", image_url: "https://t3.ftcdn.net/jpg/05/00/80/80/240_F_500808027_ifTfsf5LxuErtDjGnA1IopwF6B0ITQPT.jpg", quantity: nil},
+  {name: "improved_oil_heater", description: "heats oil", price: "200.0", image_url: "https://i5.walmartimages.com/asr/532e2708-be9a-4df8-854e-776ef1937389.82335f658c31c7055894278a30f6f9e7.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF", quantity: nil},
+  {name: "improved_oil_heater", description: "wipes spills", price: "5.0", image_url: "https://t3.ftcdn.net/jpg/05/00/80/80/240_F_500808027_ifTfsf5LxuErtDjGnA1IopwF6B0ITQPT.jpg", quantity: nil}
+])
